ed024a32e195810292005c4dbf643ec3b52f5074
Ivar Nilsen <post@ivarnilsen.com>
2017-01-28 13:35:20 +0000

    Create repo

---
+++ README.md
@@ -0,0 +1,73 @@
+# xml-edit
+
+## wat
+
+Parse an XML file to JSON object, modify text nodes and write it to a stream
+
+## why
+
+I found myself needing to update text nodes in a maven pom from a node project
+
+## how
+
+Given a file that looks like this
+```
+<this>
+    <is>
+        <yet.another>
+            XML file
+        </yet.another>
+    </is>
+</this>
+```
+
+```javascript
+const path = require('path');
+const xmlEdit = require('xml-edit');
+
+const editor = xmlEdit.getInstance({
+    indentation: 2,
+    strict: true,
+});
+
+const xmlObject = await editor.read(path.resolve('test.xml'));
+/*
+    {
+      "this": {
+        "is": {
+          "another": "XML file"
+        }
+      }
+    }
+*/
+xmlObject.this.is.another = 'ohai';
+await editor.write(path.resolve('test.xml'), xmlObject, process.stdout);
+```
+
+## but...?
+
+### getInstance(opts)
+
+_opts.indentation_: number of spaces to use when writing (default: `2`)
+_opts.strict_: parse XML strictly or not (default: `true`)
+
+### instance.read(filePath)
+
+_filePath_: Path to the file (doh)
+
+### instance.write(filePath, xmlObject, stream)
+
+_filePath_: Path to the original file
+_xmlObject_: Specification of which text nodes to change
+_stream_: A writeable stream that the result gets sent to
+
+## ok, so...?
+
+* Original indentation in the file is not respected
+* You can't edit attributes, in fact they don't even get returned by `read`
+* You can't edit tagnames
+* Your attributes *will* be inlined in the tag in the result
+
+This has not been tested very thoroughly so there are probably bugs. Let me know and I'll look at it. For now consider this a beta at best.
+
+Unit tests are coming.
---
+++ src/xml-edit.js
@@ -0,0 +1,106 @@
+import sax from 'sax';
+import { createReadStream } from 'fs';
+
+const _write = (opts, filePath, definition, stream) => {
+    const print = (chars) => {
+        stream.write(chars);
+    };
+
+    const indent = (level, indentation) => {
+        print('\n');
+        for (let i = 0, l = level * indentation; i < l; i++) {
+            print(' ');
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        const saxStream = sax.createStream(
+            opts.strict || true,
+            { trim: true },
+        );
+        const indentation = opts.indentation || 2;
+        const breadcrumbs = [];
+        let current = {};
+        let level = 0;
+
+        saxStream.on('error', e => reject(e));
+
+        saxStream.on('opentag', (el) => {
+            current[el.name] = {};
+            breadcrumbs.push(current);
+            current = current[el.name];
+
+            indent(level, indentation);
+            level += 1;
+
+            print(`<${el.name}`);
+            Object.keys(el.attributes).forEach((key) => {
+                print(` ${key}="${el.attributes[key]}"`);
+            });
+            print('>');
+        });
+
+        saxStream.on('closetag', (name) => {
+            current = breadcrumbs.pop();
+            level -= 1;
+            indent(level, indentation);
+            print(`</${name}>`);
+        });
+
+        saxStream.on('text', (text) => {
+            indent(level, indentation);
+            const content = breadcrumbs.reduce(
+                (curr, next) => curr[Object.keys(next)[0]] || curr,
+                definition,
+            );
+            print(content || text);
+        });
+
+        saxStream.on('end', () => {
+            print('\n');
+            resolve();
+        });
+
+        createReadStream(filePath).pipe(saxStream);
+    });
+};
+
+const _read = (opts, filePath) =>
+    new Promise((resolve, reject) => {
+        const saxStream = sax.createStream(
+            opts.strict || true,
+            { trim: true },
+        );
+
+        const result = {};
+        const breadcrumbs = [];
+        let current = result;
+
+        saxStream.on('error', e => reject(e));
+
+        saxStream.on('opentag', (el) => {
+            current[el.name] = {};
+            breadcrumbs.push(current);
+            current = current[el.name];
+        });
+
+        saxStream.on('closetag', () => {
+            current = breadcrumbs.pop();
+        });
+
+        saxStream.on('text', (text) => {
+            const tag = breadcrumbs[breadcrumbs.length - 1];
+            tag[Object.keys(tag)[0]] = text;
+        });
+
+        saxStream.on('end', () => {
+            resolve(result);
+        });
+
+        createReadStream(filePath).pipe(saxStream);
+    });
+
+export const getInstance = opts => ({
+    read: filePath => _read(opts, filePath),
+    write: (filePath, definition, stream) => _write(opts, filePath, definition, stream),
+});
